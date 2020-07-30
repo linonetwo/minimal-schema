c8a176983b3c14bc78bd2fbdb24cbd326faee691
Yuri Tkachenko <yuri@ya.ru>
2020-07-30 11:42:11 +0300

    better mocha

--- .mocharc.js
+++ .mocharc.js
@@ -2,6 +2,8 @@
 
 module.exports = {
   diff: true,
+  'watch-files': ['test/**/*.ts', 'src/**/*.ts'],
+  recursive: ['test/**/*.ts'],
   package: './package.json',
   reporter: 'nyan',
   slow: 75,
